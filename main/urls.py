2f9d1fc6ac30e85799052e8fe32afe731b8df4d5
Richard Rozario A <richardrozario2000@gmail.com>
2020-07-01 22:08:46 +0530

    modifying urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [path('home/',views.Students, name='Students'),
-			path('reviews/',views.Reviews, name='Reviews')
+urlpatterns = [
+			path('',views.Students, name='Students'),
+			path('home/',views.Students, name='Students'),
+			path('reviews/',views.Reviews, name='Reviews'),
 ]
